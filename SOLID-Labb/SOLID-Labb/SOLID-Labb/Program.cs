7c11d671bb5b4822e73eed3b7db1de324b6378de
Natoorr <54800494+Natoorr@users.noreply.github.com>
2021-01-22 14:46:13 +0100

    a

--- SOLID-Labb/SOLID-Labb/SOLID-Labb/Program.cs
+++ SOLID-Labb/SOLID-Labb/SOLID-Labb/Program.cs
@@ -6,30 +6,50 @@ namespace SOLID_Labb
     {
         static void Main(string[] args)
         {
-            Animal[] animals = {new Dog("Black"),
-                            new Hedgehog("Pink"),
-                            new Bird("Yellow")};
+            Animal[] animals = animalColor();
 
             Console.WriteLine("###############");
             Console.WriteLine();
 
+            Temp(animals);
+
+            Console.WriteLine("###############");
+            Console.WriteLine();
+
+        }
+
+        static Animal[] animalColor()
+        {
+            Animal[] animals = {new Dog("Black"),
+                            new Hedgehog("Pink"),
+                            new Bird("Yellow")};
+
+            return animals;
+        }
+
+        static void Temp(Animal[] animals)
+        {
             foreach (var animal in animals)
             {
                 if (animal is Dog dog)
                 {
                     dog.TransferOwnership("Niklas");
                 }
-
-                animal.Eat();
-                animal.Sleep();
-                animal.Speak();
-                animal._color = "Grey";
-                Console.WriteLine(animal.GetType().Name + " is " + animal._color);
-                Console.WriteLine();
-
-                Console.WriteLine("###############");
-                Console.WriteLine();
+                Text(animal);
             }
+        }    
+
+        static void Text(Animal animal)
+        {
+            animal.Eat();
+            animal.Sleep();
+            animal.Speak();
+            animal._color = "Grey";
+            Console.WriteLine(animal.GetType().Name + " is " + animal._color);
+            Console.WriteLine();
+
         }
     }
+
+
 }
